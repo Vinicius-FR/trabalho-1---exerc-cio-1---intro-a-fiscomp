f23a63eb5b409e08656d2bca1e73b6070cc01b48
Domvs <viniciusfr44a@gmail.com>
2022-09-19 17:35:13 -0300

    rodando o código para diferentes x

--- exer02.f90
+++ exer02.f90
@@ -2,9 +2,11 @@ program exer02
     IMPLICIT NONE
     ! Esse é um exemplo de comentario
     integer :: fact, i, n
-    real :: x = 0.1
+    real :: x = 0.1, y = 0.2, z = 0.3, w = 0.4
     real :: seno = 0
     real :: termo
+
+    ! x = 0.1:
 
     n = 1
 
@@ -15,6 +17,72 @@ program exer02
         end do
         
         termo = (x ** (2 * n - 1)) * ((-1)**(n+1))/ fact
+        if (seno /= seno + termo) then
+            seno = seno + termo
+            n = n + 1
+        else
+            exit
+        end if
+    end do
+
+    print *, seno
+
+    ! x = y = 0.2:
+
+    n = 1
+    seno = 0
+
+    do
+        fact = 1
+        do i = 1, 2 * n - 1
+            fact = fact*i
+        end do
+        
+        termo = (y ** (2 * n - 1)) * ((-1)**(n+1))/ fact
+        if (seno /= seno + termo) then
+            seno = seno + termo
+            n = n + 1
+        else
+            exit
+        end if
+    end do
+
+    print *, seno
+
+    ! x = z = 0.3:
+
+    n = 1
+    seno = 0
+
+    do
+        fact = 1
+        do i = 1, 2 * n - 1
+            fact = fact*i
+        end do
+        
+        termo = (z ** (2 * n - 1)) * ((-1)**(n+1))/ fact
+        if (seno /= seno + termo) then
+            seno = seno + termo
+            n = n + 1
+        else
+            exit
+        end if
+    end do
+
+    print *, seno
+
+    ! x = w = 0.4:
+
+    n = 1
+    seno = 0
+
+    do
+        fact = 1
+        do i = 1, 2 * n - 1
+            fact = fact*i
+        end do
+        
+        termo = (w ** (2 * n - 1)) * ((-1)**(n+1))/ fact
         if (seno /= seno + termo) then
             seno = seno + termo
             n = n + 1
